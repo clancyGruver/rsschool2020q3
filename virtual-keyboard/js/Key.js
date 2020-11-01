a0b8f18b11d560b992e4a2d849a75a8a1c4f9e26
Burima Evgenyi <cuksmail@gmail.com>
2020-11-01 08:51:06 +0500

    feat: base functional ready

---
+++ virtual-keyboard/js/Key.js
@@ -0,0 +1,71 @@
+import create from './utils/create.js';
+
+const iconElements = [
+
+];
+
+export default class Key{
+    constructor ({ small, shift, code, icon }) {
+        this.code = code;
+        this.small = small;
+        this.shift = shift;
+        this.icon = icon || null;
+        this.classNames = ['keyboard__key'];
+        this.createKey();
+    }
+
+    setKeyBase ({ small, shift }) {
+        this.small = small;
+        this.shift = shift;
+    }
+
+    updateKey(){
+        if (this.shift && this.shift.match(/[^a-zA-Zа-яА-ЯЁё0-9]/)) {
+            this.sub.textContent = this.shift;
+        } else {
+            this.sub.textContent = '';
+        }
+        this.letter.textContent = this.small;
+    }
+
+    isFnKey () {
+        const smallMatched = Boolean(this.small.match(/Ctrl|arr|Shift|Tab|Back|Del|Enter|Caps|Win|Alt|done|EnRu/));
+        const codeMatched = Boolean(this.code.match(/done|EnRu/));
+
+        return smallMatched || codeMatched;
+    }
+
+    createKey () {
+        this.isFnKey = this.isFnKey();
+
+        if (this.icon){
+            this.iconElement = this.createIconHTML(this.icon);
+        }
+
+        if (this.shift && this.shift.match(/[^a-zA-Zа-яА-ЯЁё0-9]/)) {
+            this.sub = create('div', 'keyboard__key__sub', this.shift);
+        } else {
+            this.sub = create('div', 'keyboard__key__sub', '');
+        }
+
+        this.letter = create('div', 'letter', this.small);
+
+        if(this.code == 'Space') this.classNames.push('keyboard__key--extra-wide');
+        if(this.code == 'CapsLock') this.classNames.push('keyboard__key--activatable');
+        if(this.isFnKey) this.classNames.push('keyboard__key--functional');
+
+        this.container = create(
+            'div', 
+            this.classNames, 
+            [this.sub, this.iconElement || this.letter], 
+            null, 
+            ['code', this.code],
+            ['fn', this.isFnKey ? true : false]
+        );
+
+    }
+
+    createIconHTML(iconName){
+        return create('i', 'material-icons', iconName);
+    }
+}
---
+++ virtual-keyboard/js/Keyboard.js
@@ -0,0 +1,234 @@
+import { get, set } from './storage.js';
+import create from './utils/create.js';
+import language from './langs/index.js';
+import Key from './Key.js';
+
+const main = create('main', '');
+
+export default class Keyboard {
+    constructor (rowsOrder) {
+        this.rowsOrder = rowsOrder;
+        this.keyPressed = {};
+        this.isCaps = false;
+    }
+
+    init (langCode) {
+        this.keyBase = language[langCode];
+        this.output = document.querySelector('.keyboard-input');
+        this.container = create('div', 'keyboard', null, main, ['language', langCode]);
+        document.body.prepend(main);
+        this.hide();
+        return this;
+    }
+
+    generateLayout () {
+        this.keyButtons = [];
+        this.rowsOrder.forEach( (row, i) => {
+            const rowElement = create('div', 'keyboard__row keyboard__keys', null, this.container, ['row', i+1]);
+            row.forEach( code => {
+                const keyObject = this.keyBase.find( key => key.code === code);
+                if (keyObject) {
+                    const keyButton = new Key(keyObject);
+                    this.keyButtons.push(keyButton);
+                    rowElement.appendChild(keyButton.container);
+                }
+            } );
+        });
+
+        document.addEventListener('keydown', this.handleEvent);
+        document.addEventListener('keyup', this.handleEvent);
+        this.container.addEventListener('mousedown', this.prehandleEvent);
+        this.container.addEventListener('mouseup', this.prehandleEvent);
+    }
+
+    prehandleEvent = (e) => {
+        e.stopPropagation();
+        const keyDiv = e.target.closest('.keyboard__key');
+        if(!keyDiv) return;
+        const { dataset: { code } } = keyDiv;
+        keyDiv.addEventListener('mouseLeave', this.resetButtonState);
+        this.handleEvent({ code, type: e.type });
+    }
+
+    handleEvent = (e) => {
+        if (e.stopPropagation) e.stopPropagation();
+        const {code, type } = e;
+        const keyObj = this.keyButtons.find( key => key.code === code );
+        if(!keyObj) return;
+        this.output.focus();
+        if (type.match(/keydown|mousedown/)){
+            if (type.match(/keydown/)) e.preventDefault();
+
+            keyObj.container.classList.add('keyboard__key--press');
+
+            //functional keys 
+            if (code.match(/Control/)) this.ctrlKey = true;
+            if (code.match(/Alt/)) this.altKey = true;
+            if (code.match(/Shift/)) this.shiftKey = true;
+            if (code.match(/Caps/)) this.handleCapsLock(keyObj);
+
+            if (this.shiftKey) this.switchUpperCase(true);
+
+            //change language
+            if (code.match(/EnRu/)) this.switchLanguage();
+
+            //output
+            if(!this.isCaps){
+                this.printToOutput(keyObj, this.shiftKey ? keyObj.shift : keyObj.small);
+            } else if (this.isCaps) {
+                if(this.shiftKey){
+                    this.printToOutput(keyObj, this.shiftKey ? keyObj.shift : keyObj.small);
+                } else {
+                    this.printToOutput(keyObj, !keyObj.sub.innerHTML ? keyObj.shift : keyObj.small);
+                }
+            }
+        } else if (type.match(/keyup|mouseup/)) {
+            //functional keys 
+            if(code.match(/Control/)) this.ctrlKey = false;
+            if(code.match(/Alt/)) this.altKey = false;
+            if(code.match(/Shift/)) {
+                this.shiftKey = false;
+                this.switchUpperCase(false);
+            }
+
+            keyObj.container.classList.remove('keyboard__key--press');
+        }
+    }
+
+    handleCapsLock (keyObj) {
+        if(!this.isCaps === true) {
+            this.isCaps = true;
+            this.switchUpperCase(true);
+            keyObj.container.classList.add('keyboard__key--active');
+        } else {
+            this.isCaps = false;
+            this.switchUpperCase(false);
+            keyObj.container.classList.remove('keyboard__key--active');
+        }
+    }
+
+    show () {
+        this.container.classList.remove('keyboard--hidden');
+    }
+
+    hide () {
+        this.output.blur();
+        this.container.classList.add('keyboard--hidden');
+    }
+
+    switchLanguage = () => {
+        const langAbbr = Object.keys(language);
+        const currentLangIdx = langAbbr.indexOf(this.container.dataset.language);
+        const nextLangIdx = currentLangIdx + 1 < langAbbr.length ? currentLangIdx + 1 : 0;
+        const newLang = langAbbr[nextLangIdx];
+        this.keyBase = language[newLang];
+
+        this.container.dataset.language = newLang;
+        set('kbLang', newLang);
+
+        this.keyButtons.forEach( keyBtn => {
+            const keyObject = this.keyBase.find( key => key.code === keyBtn.code );
+            if(!keyObject) return;
+            keyBtn.setKeyBase(keyObject);
+            keyBtn.updateKey();
+        })
+
+        if (this.isCaps) this.switchUpperCase(true);
+    }
+
+    switchUpperCase (isTrue) {
+        if(isTrue) {
+            this.keyButtons.forEach( button => {
+                if (button.sub.innerHTML) {
+                    if (this.shiftKey) {
+                        button.sub.classList.add('keyboard__key__sub--active');
+                        button.letter.classList.add('keyboard__key--inactive');
+                    }
+                }
+
+                if(!button.isFnKey && this.isCaps && !this.shiftKey && !button.sub.innerHTML){
+                    button.letter.textContent = button.shift;
+                } else if (!button.isFnKey && this.isCaps && this.shiftKey) {
+                    button.letter.textContent = button.small;
+                } else if (!button.isFnKey && !button.sub.innerHTML) {
+                    button.letter.textContent = button.shift;
+                }
+            })
+        } else {
+            this.keyButtons.forEach( button => {
+                if (!button.isFnKey && button.sub.innerHTML) {
+                    button.sub.classList.remove('keyboard__key__sub--active');
+                    button.letter.classList.remove('keyboard__key--inactive');
+
+                    if (!this.isCaps) button.letter.textContent = button.small;
+                    else if (this.isCaps) button.letter.textContent = button.shift;
+                } else if (!button.isFnKey) {
+                    if(this.isCaps) {
+                        button.letter.textContent = button.shift;
+                    } else {
+                        button.letter.textContent = button.small;
+                    }
+
+                }
+            })
+        }
+
+    }
+
+    printToOutput (keyObj, symbol) {
+        let cursorPosition = this.output.selectionStart;
+        const left = this.output.value.slice(0, cursorPosition);
+        const right = this.output.value.slice(cursorPosition);
+
+        const fnButtonsHandler = {
+            Tab: () => {
+                this.output.value = `${left}\t${right}`;
+                cursorPosition++;
+            },
+            ArrowLeft: () => cursorPosition -= cursorPosition > 0 ? 1 : 0,
+            ArrowRight: () => cursorPosition++,
+            ArrowUp: () => {
+                const positionFromLeft = this.output.value.slice(0, cursorPosition).match(/(\n).*$(?!\1)/g) || [[1]];
+                cursorPosition -= positionFromLeft[0].length;
+            },
+            ArrowDown: () => {
+                const positionFromLeft = this.output.value.slice(0, cursorPosition).match(/(\n).*$(?!\1)/g) || [[1]];
+                cursorPosition += positionFromLeft[0].length;
+            },
+            Enter: () => {
+                this.output.value = `${left}\n${right}`;
+                cursorPosition++;
+            },
+            Delete: () => this.output.value = `${left}${right.slice(1)}`,
+            Backspace: () => {
+                this.output.value = `${left.slice(0, -1)}${right}`;
+                cursorPosition--;
+            },
+            Space: () => {
+                this.output.value = `${left} ${right}`;
+                cursorPosition++;
+            },
+            done: () => {
+                this.hide();
+                return;
+            },
+        }
+
+        if (fnButtonsHandler[keyObj.code]) fnButtonsHandler[keyObj.code]();
+        else if (!keyObj.isFnKey){
+            cursorPosition++;
+            this.output.value = `${left}${symbol || ''}${right}`;
+        }
+        this.output.setSelectionRange(cursorPosition, cursorPosition);
+    }
+
+    resetPressedButtons = (targetCode) => {
+        if (!this.keysPressed[targetCode]) return;
+        if (!this.isCaps) this.keysPressed[targetCode].div.classList.remove('keyboard__key--press');
+        this.keysPressed[targetCode].container.removeEventListener('mouseleave', this.resetButtonState);
+        delete this.keysPressed[targetCode];
+    }
+}
+
+
+//todo: hidekeyboard long press
