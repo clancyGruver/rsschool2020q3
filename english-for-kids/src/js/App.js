554a05c543b48db5b5c606f7510d0be04d4707e0
Burima Evgenyi <cuksmail@gmail.com>
2020-11-22 21:35:03 +0500

    ref: cards moved to JSON fix: rounded top borders at card feat: added App class

---
+++ english-for-kids/src/js/App.js
@@ -0,0 +1,15 @@
+export default class App {
+  MODES = {
+    TRAIN: 1,
+    PLAY: 2,
+  };
+
+  constructor() {
+    this.mode = this.MODES.TRAIN;
+    this.load();
+  }
+
+  load() {
+    
+  }
+}
