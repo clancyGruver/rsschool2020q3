00b9389a7b561df51c0d37e1fb9c74c93e7b7cc0
Evgenyi <cuksmail@gmail.com>
2020-12-22 21:57:16 +0500

    feat: fullscreen started

--- covid-19/src/components/FullScreen.js
+++ covid-19/src/components/FullScreen.js
@@ -1,26 +1,35 @@
 import React from 'react';
-// import Full from '../assets/images/full-screen.svg';
-// import minimize from '../assets/images/minimize.svg';
+import ToFullScreen from '../assets/images/tofullscreen.svg';
+import Minimize from '../assets/images/minimize.svg';
+import Style from '../styles/FullScreen.module.css';
 
 export default class FullScreen extends React.Component {
   constructor(props){
     super(props)
     this.state = {
       isFullScreen: true,
+      rowElement: null,
     }
   }
 
-  clickHandler() {
+  clickHandler(e) {
     const isFullScreen = !this.state.isFullScreen;
     this.setState({ isFullScreen });
+    const rowElement = e.target.closest('.row');
+    this.setState({ rowElement }, this.changeClass());
   }
-  // {}this.state.isFullScreen
-  // : <minimize onClick={this.clickHandler} />
-  
+
+  changeClass(){
+    if(this.state.rowElement) {
+      this.state.rowElement.classList.toggle('row');
+      this.state.rowElement.classList.toggle('full-screen');
+    }
+  }
+
   render() {
-    return (
-      // <Full onClick={this.clickHandler} />
-      <p>fixme</p>
-    );
+    const el = this.state.isFullScreen ?
+      <img src={ToFullScreen} alt="to full screen" className={Style.icon} onClick={(e)=>this.clickHandler(e)} /> :
+      <img src={Minimize} alt="minimize screen" className={Style.icon} onClick={(e)=>this.clickHandler(e)} />
+    return el;
   }
 }
